796d40edb73ebdb69a4f7673365278d2f556e497
TinTDang <tin2dang@gmail.com>
2018-08-21 17:20:49 -0500

    added required document access

---
+++ letter.js
@@ -0,0 +1,5 @@
+// constructor that displays an underlying character or a blank placeholder based on if the user has or has not guessed the letter
+// string value that stores the underlying character ofr the letter
+// boolean value that stores whether the letter has been guessed yet
+// function that returns the underlying character if the letter has been guessed, or a placeholder if the letter has not been guessed
+// function that takes a character as an argument and checks it against the underlying character updating the stored boolean value to true if it was guessed correctly.
